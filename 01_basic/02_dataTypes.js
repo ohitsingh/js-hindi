bcdd5e9f7d1cb16d429a862d65874e037b0616ff
Mohit Kumar <mohitkrsingh8757@gmail.com>
2025-06-08 05:43:08 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3+4)
+
+console.log(3+3)
+
+let name = "Mohit"
+let age = 18
+let isLoggeedIn = false
+
+// number => 2 
+// bigint 
+//String ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symplole unique
+//object
+
+console.log(typeof name )
+console.log(typeof undefined)
+console.log(typeof null) // object
