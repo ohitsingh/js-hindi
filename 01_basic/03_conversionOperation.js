9cd1d184eee44b77f4aa745102c9e597e2a3c8f3
Mohit Kumar <mohitkrsingh8757@gmail.com>
2025-06-08 05:56:07 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" 33
+// "33abc" NaN
+// 1 -> true
+// 0 -> false
+// "" -> false
+// "hitesh" -> true
