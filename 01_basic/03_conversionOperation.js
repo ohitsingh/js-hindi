034452b309fb6fbef0f9caca3044e8cbcb5f7e33
Mohit Kumar <mohitkrsingh8757@gmail.com>
2025-06-08 06:31:23 +0000

    data type summery

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33abc"
-console.log(typeof score)
-console.log(typeof(score))
+// let score = "33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" 33
 // "33abc" NaN
@@ -12,3 +12,17 @@ console.log(valueInNumber)
 // 0 -> false
 // "" -> false
 // "hitesh" -> true
+
+
+// operation
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = "mohit"
+// let str3 = str1+str2
+// console.log(str3)
+
+console.log(+true)
---
+++ 01_basic/datatype-summery.js
@@ -0,0 +1,37 @@
+// Primitive
+
+
+// 7
+// String, Number, Boolean,null,undefined,Symbol,BigInt
+
+// Refrence type(non primitive)
+
+// Array, Object,function
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggeedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.table([id,anotherId])
+console.log(id === anotherId)
+
+
+// const bigNumber = 324558976897656
+
+const heros = ["mohit","naagraj","doga"]
+
+let myObj = {
+    name : "Mohit",
+    age : 18
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
