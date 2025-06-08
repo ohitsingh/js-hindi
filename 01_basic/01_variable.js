9c96ef57dded3e184e1b5a103cd06a2ec4e21e40
Mohit Kumar <mohitkrsingh8757@gmail.com>
2025-06-08 05:43:43 +0000

    data types

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 14456
+let accountEmail = "mohit@gamial.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+// accountId = 2;
+
+accountEmail = "m@gamil.com"
+accountPassword = "24423"
+accountCity = "BLR"
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword])
+
+/*
+*/
