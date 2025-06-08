c6241d492390f9ce68a6592b52cbb38720580d9e
Mohit Kumar <mohitkrsingh8757@gmail.com>
2025-06-08 10:16:41 +0000

    Math and Num

---
+++ 01_basic/05_string.js
@@ -0,0 +1,26 @@
+const name = "Mohit"
+const repoCount = 50
+
+// console.log(name+repoCount+"value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('mohit-m')
+// console.log(gameName[0])
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const gameName1 = "  mohit       "
+console.log(gameName1)
+console.log(gameName1.trim())
+
+console.log(gameName.split('-'))
+
